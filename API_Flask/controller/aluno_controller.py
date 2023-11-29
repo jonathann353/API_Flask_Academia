0d8d733627bda1899221c2d1feaed0bb60614b76
Jonathan Nunes <82113802+jonathann353@users.noreply.github.com>
2023-11-29 20:52:03 -0300

    comentada a linha de user test

--- API_Flask/controller/aluno_controller.py
+++ API_Flask/controller/aluno_controller.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_requir
 from hashlib import sha256
 #from model.connection import conn ##conexão com o workbench 
 from model.db_postgres import conn ##conexão com o postgres 
-from model.db_admin import UserTest ##descomente está linha para usar o usuário de teste
+#from model.db_admin import UserTest ##descomente está linha para usar o usuário de teste
 
 MY_APP = Blueprint('MY_APP', __name__)#link do controller com a main
 
